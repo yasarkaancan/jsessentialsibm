7e9d762b80540a3409d84854b1591167ceebf5aa
yasarkaancan <yasarkaancan@hotmail.com>
2025-02-01 21:11:53 +0300

    added delete feature

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -25,7 +25,9 @@ function showbooks() {
         <p><strong>Book Name: </strong>${book.name}</p>
         <p><strong>Author Name:</strong> ${book.authorName}</p>
         <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>
+        <button onclick='deleteBook(${index})'>Delete</button>
+        `
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
@@ -35,4 +37,9 @@ function clearInputs() {
     document.getElementById('authorName').value = '';
     document.getElementById('bookDescription').value = '';
     document.getElementById('pagesNumber').value = '';
+}
+
+function deleteBook(index) {
+    books.splice(index, 1)
+    showbooks()
 }
